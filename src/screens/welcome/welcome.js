7319e6835b10037d45cb8f09b6849104503968da
ggDev95 <devotiluca95@gmail.com>
2022-04-21 10:16:24 +0200

    CSS welcome page e bottone

--- src/screens/welcome/welcome.js
+++ src/screens/welcome/welcome.js
@@ -13,32 +13,32 @@ function Welcome() {
     }
     return (
         <>
-            <header></header>
-            <main>
-                <div className='title_container'>
-                    <h1>I.H.S.P</h1>
-                    <h2>Incredible Hard Second Project</h2>
-                    <h4>La pasquetta peggiore della tua vita</h4>
-                </div>
-                <div className='button_container'>
-                    <UiButton
-                        label={'Gioca'}
-                        path={"/game"}
-                        callback={goToPage}
-                    />
-                    <UiButton
-                        label={'Tutorial'}
-                        path={"/tutorial"}
-                        callback={goToPage}
-                    />
-                    <UiButton
-                        label={'Classifica'}
-                        path={"/leaderboard"}
-                        callback={goToPage} />
+            <div className='container'>
 
-                </div>
-            </main>
-            <footer></footer>
+                <header></header>
+                <main>
+                    <div className='title_container'>
+                        <h1>I.H.S.P</h1>
+                        <h2>Incredible Hard Second Project</h2>
+                        <h4>La pasquetta peggiore della tua vita</h4>
+                    </div>
+
+                    <div className='button_container'>
+                        <UiButton
+                            label={'Gioca'}
+                            path={"/game"}
+                            callback={goToPage}
+                        />
+                        
+                        <UiButton
+                            label={'Score'}
+                            path={"/leaderboard"}
+                            callback={goToPage} />
+
+                    </div>
+                </main>
+                <footer>NLD production, all right reserved</footer>
+            </div>
         </>
     )
 }
